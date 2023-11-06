2bcf17e962dce325f99f43d1e7694c56e8b439b9
VerikPaster <verikpaster22@gmail.com>
2023-11-06 15:11:53 +0100

    Second Commit

--- Assets/Scripts/GameStateHandler.cs
+++ Assets/Scripts/GameStateHandler.cs
@@ -1,21 +1,32 @@
-﻿using UnityEngine;
-using Zenject;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStateHandler : MonoBehaviour
 {
     [SerializeField] private GameObject StartGameScreen; //референс на цей обєкт в сцені
     [SerializeField] private GameObject MainGameScreen; //референс на цей обєкт в сцені
-    //public event Action<GameState> OnGameStateChanged;
+    [SerializeField] private Button startButton;
+    public event Action<GameState> OnGameStateChanged;
 
     private void Start()
     {
-        UpdateGameStates(GameState.StartGame);
+        UpdateGameStates(GameState.StartGame); 
+        startButton.onClick.AddListener(StartGame);
+    }
+    
+    public void StartGame()
+    {
+        //Міняємо стан гри кнопкою
+        UpdateGameStates(GameState.GameStarted);
     }
 
-    public void UpdateGameStates(GameState state)
+    private void UpdateGameStates(GameState state)
     {
         StartGameScreen.SetActive(state == GameState.StartGame); //Показуємо стартову панель
         MainGameScreen.SetActive(state == GameState.GameStarted); //Показуємо основну панель
+        OnGameStateChanged?.Invoke(state);
     }
 }
 public enum GameState
